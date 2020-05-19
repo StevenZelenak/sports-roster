f9e336b0da7d6190d24da91c803889e9efaa1f06
Steven Zelenak <stevenmzelenak@gmail.com>
2020-05-18 20:32:57 -0500

    wip

---
+++ src/components/MyNavbar/MyNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+        <h1>Inside Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
---
+++ src/components/TeamComponent/TeamComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TeamComponent.scss';
+
+class TeamComponent extends React.Component {
+  render() {
+    return (
+      <div className="TeamComponent">
+        <h1>Inside the TeamComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default TeamComponent;
